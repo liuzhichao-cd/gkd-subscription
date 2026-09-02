1b48b46dc9775de3fe2ad8e89e49a7e91f485e07
liuzhichao <zhichao.liu@gh-ca.com>
2026-09-02 20:44:37 +0800

    feat: 新增量子密信规则

---
+++ src/apps/com.ctq.qmchat.ts
@@ -0,0 +1,26 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.ctq.qmchat',
+  name: '量子密信',
+  groups: [
+    {
+      key: 1,
+      name: '权限提示',
+      matchTime: 10000,
+      activityIds: 'com.ctq.modulemain.ui.MainActivity',
+      rules: [
+        {
+          key: 0,
+          name: '关闭消息权限提示',
+          fastQuery: true,
+          matches: 'TextView[text="取消"||text="我知道了"]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/31737540',
+            'https://i.gkd.li/i/31737509',
+          ],
+        },
+      ],
+    },
+  ],
+});
